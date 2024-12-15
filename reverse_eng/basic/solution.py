9519056dfac61f35a5f0df80dd60dc15ec7c2fc6
Rezki <94495429+Rezkiii@users.noreply.github.com>
2024-12-15 15:56:08 +0800

    Delete reverse_eng directory

--- reverse_eng/basic/solution.py
+++
@@ -1,9 +0,0 @@
-def deobfuscate_flag(obfuscated):
-    key = 42  # Same XOR key
-    deobfuscated = ''.join(chr(ord(c) ^ key) for c in obfuscated)
-    return deobfuscated
-
-if __name__ == "__main__":
-    obfuscated_flag = "F#\x02\"IKZY^EDOQXO\OXYOuODMCDOOXCDMuHKYCIW"  # Example output from challenge.py
-    original_flag = deobfuscate_flag(obfuscated_flag)
-    print("Original Flag:", original_flag)
